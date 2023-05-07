dcfcbe36bab2cd5935a4481b67d5c907dc484dce
ange stephane <boguhestephane97@gmail.com>
2023-05-07 15:00:21 +0100

    router

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
 
+import router from "./router";
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.status(200);
   res.json({ message: "OK" });
 });
+
+app.use("/api/v1", router);
 
 export default app;
