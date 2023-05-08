a5927fe3b73237661644f413d7ec94e71831c169
ange stephane <boguhestephane97@gmail.com>
2023-05-08 23:01:42 +0100

    add morgan logged and cors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
 import express from "express";
-
+import morgan from "morgan";
+import cors from "cors";
 import router from "./router";
 
 const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Permet d'utiliser les URLs cryptée (google.com?q=1&b=2)
 
 app.get("/", (req, res) => {
   res.status(200);
