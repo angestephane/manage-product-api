fdc0c00528698a7eed2ba5db27cb3e9439c7dc06
ange stephane <boguhestephane97@gmail.com>
2023-05-16 09:51:36 +0100

    product handle
    create product CRUD

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,6 +1,75 @@
 import { Request, Response } from "express";
+import prisma from "../db";
 
-const createProduct = (req: Request, res: Response) => {};
-const updateProduct = (req: Request, res: Response) => {};
+// Get all products of the current user
+const getProducts = async (req: Request, res: Response) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.user.id,
+    },
+    include: {
+      products: true,
+    },
+  });
 
-export { createProduct, updateProduct };
+  return res.json({ data: user.products });
+};
+
+// get one product of the current user
+const getProduct = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const product = await prisma.product.findFirst({
+    where: {
+      id,
+      authorId: req.user.id,
+    },
+  });
+
+  return res.json({ data: product });
+};
+
+// let current user to create a product
+const createProduct = async (req: Request, res: Response) => {
+  const product = await prisma.product.create({
+    data: {
+      name: req.body.name,
+      authorId: req.user.id,
+    },
+  });
+
+  return res.json({ data: product });
+};
+
+// let current user to update a product
+const updateProduct = async (req: Request, res: Response) => {
+  const newProduct = await prisma.product.update({
+    where: {
+      id_authorId: {
+        id: req.params.id,
+        authorId: req.user.id,
+      },
+    },
+    data: {
+      name: req.body.name,
+    },
+  });
+
+  return res.json({ data: newProduct });
+};
+
+// let current user to delete a product
+const deleteProduct = async (req: Request, res: Response) => {
+  const productDeleted = await prisma.product.delete({
+    where: {
+      id_authorId: {
+        id: req.params.id,
+        authorId: req.user.id,
+      },
+    },
+  });
+
+  return res.json({ data: productDeleted });
+};
+
+export { getProducts, getProduct, createProduct, updateProduct, deleteProduct };
