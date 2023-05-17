ed4ba40ab71e3853a41037dfdb830a819ef43140
ange stephane <boguhestephane97@gmail.com>
2023-05-17 16:18:27 +0100

    setup unit test

---
+++ src/handlers/__test__/user.test.ts
@@ -0,0 +1,7 @@
+import { createUser } from "../user";
+
+describe("user handler Unit test", () => {
+  it("should successfully generate token and add user in db if username and password are correct", async () => {
+    expect(200).toEqual(200);
+  });
+});
